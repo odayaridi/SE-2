c8efa6c4d57d45a25e40a7ceeec31f67e83819c8
odayaridi <odayaridi.se@gmail.com>
2026-07-14 20:50:44 +0300

    Applied solid principles on orders example

---
+++ src/app.ts
@@ -0,0 +1,52 @@
+import {
+    FinanceCalculator,
+    ItemValidator,
+    MaxPriceValidator,
+    OrderManagement,
+    PriceValidator,
+    Validator
+} from "./app-clean";
+
+const orders = [
+  { id: 1, item: "Sponge", price: 15 },
+  { id: 2, item: "Chocolate", price: 20 },
+  { id: 3, item: "Fruit", price: 18 },
+  { id: 4, item: "Red Velvet", price: 25 },
+  { id: 5, item: "Coffee", price: 0 },
+];
+
+const rules = [
+  new PriceValidator(),
+  new MaxPriceValidator(),
+  new ItemValidator()
+];
+
+const orderManager = new OrderManagement(new Validator(rules), new FinanceCalculator());
+
+for (const order of orders) {
+  orderManager.addOrder(order.item, order.price);
+}
+
+// Adding a new order directly
+const newItem = "Marble";
+const newPrice = 22;
+
+orderManager.addOrder(newItem, newPrice);
+
+console.log("Orders after adding a new order", orderManager.getOrders());
+
+// Calculate Total Revenue directly
+console.log("Total Revenue: ", orderManager.getTotalRevenue());
+
+// Calculate Average Buy Power directly
+console.log("Average Buy Power:", orderManager.getBuyPower());
+
+// Fetching an order directly
+const fetchId = 2;
+const fetchedOrder = orderManager.getOrder(fetchId);
+console.log("Order with ID 2:", fetchedOrder);
+
+// Attempt to fetch a non-existent order
+const nonExistentId = 10;
+const nonExistentOrder = orderManager.getOrder(nonExistentId);
+console.log("Order with ID 10 (non-existent):", nonExistentOrder);
